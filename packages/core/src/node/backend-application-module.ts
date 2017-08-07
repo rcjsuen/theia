c7447cadd431130773b572d8de5fca638fae3eb9
Anton Kosiakov <anton.kosyakov@typefox.io>
2017-08-07 20:31:06 +0200

    [extension-system] use cluster module for restarting
    Signed-off-by: Anton Kosiakov <anton.kosyakov@typefox.io>

--- packages/core/src/node/backend-application-module.ts
+++ packages/core/src/node/backend-application-module.ts
@@ -5,24 +5,19 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import { ContainerModule } from "inversify";
-import { bindContributionProvider, ConnectionHandler, JsonRpcConnectionHandler, MessageService } from '../common';
-import { MessageClient, DispatchingMessageClient, messageServicePath } from '../common/message-service-protocol';
+import { ContainerModule, interfaces } from "inversify";
+import { bindContributionProvider } from '../common/contribution-provider';
 import { BackendApplication, BackendApplicationContribution } from "./backend-application";
+import { ServerProcess, RemoteMasterProcessFactory, clusterRemoteMasterProcessFactory } from './cluster';
+
+export function bindServerProcess(bind: interfaces.Bind, masterFactory: RemoteMasterProcessFactory): void {
+    bind(RemoteMasterProcessFactory).toConstantValue(masterFactory);
+    bind(ServerProcess).toSelf().inSingletonScope();
+}
 
 export const backendApplicationModule = new ContainerModule(bind => {
     bind(BackendApplication).toSelf().inSingletonScope();
     bindContributionProvider(bind, BackendApplicationContribution);
 
-    bind(DispatchingMessageClient).toSelf().inSingletonScope();
-    bind(MessageClient).toDynamicValue(ctx => ctx.container.get(DispatchingMessageClient)).inSingletonScope();
-    bind(ConnectionHandler).toDynamicValue(ctx =>
-        new JsonRpcConnectionHandler<MessageClient>(messageServicePath, client => {
-            const dispatching = ctx.container.get<DispatchingMessageClient>(DispatchingMessageClient);
-            dispatching.clients.add(client);
-            client.onDidCloseConnection(() => dispatching.clients.delete(client));
-            return dispatching;
-        })
-    ).inSingletonScope();
-    bind(MessageService).toSelf().inSingletonScope();
+    bindServerProcess(bind, clusterRemoteMasterProcessFactory);
 });
